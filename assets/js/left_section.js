4988177dcaf98acd0c459312cca249000e2e6bca
Amandeep Singh <109553527+Amanespana@users.noreply.github.com>
2023-10-29 04:44:29 -0500

    added js

---
+++ assets/js/left_section.js
@@ -0,0 +1,23 @@
+leftSectionContent = `
+<header id="header">
+      <div class="inner">
+        <a href="#" class="image avatar"
+          ><img src="images/5.png" alt="Profile Pic"
+        /></a>
+        <h1><strong>Amandeep Singh</strong><br /></h1>
+        <p>
+          Data Analyst<br />
+          <br />
+          👋 Hey! Welcome to my portfolio. I'm a Data Analyst. I have a Master's
+          degree in Mechanical Engineering from Concordia University and an IBM
+          Professional Data Analyst Certificate, where I honed my analytical and
+          problem-solving skills.
+          <br />
+          <br />
+          <b>Top Skills: </b> Python | SQL | Excel | Power BI
+        </p>
+      </div>
+    </header>
+    `
+
+    document.getElementById('left-section-container').innerHTML = leftSectionContent;
