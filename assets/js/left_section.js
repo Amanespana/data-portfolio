fd3167787929381fa9ed6ed42c36fd5e129fb4a5
Amanespana <amandeep1552@gmail.com>
2023-11-24 19:06:19 -0500

    add tesla stock prediction

--- assets/js/left_section.js
+++ assets/js/left_section.js
@@ -5,10 +5,10 @@ leftSectionContent = `
           ><img src="images/5.png" alt="Profile Pic"
         /></a>
         <h1><strong>Amandeep Singh</strong><br /></h1>
-        <p>
-          Data Analyst<br />
+        <p style="text-align: center;">
+          Data Scientist @ Leveragai<br />
           <br />
-          👋 Hey! Welcome to my portfolio. I'm a Data Analyst. I have a Master's
+          👋 Hey! Welcome to my portfolio. I'm a Data Scientist. I have a Master's
           degree in Mechanical Engineering from Concordia University and an IBM
           Professional Data Analyst Certificate, where I honed my analytical and
           problem-solving skills.
@@ -16,6 +16,33 @@ leftSectionContent = `
           <br />
           <b>Top Skills: </b> Python | SQL | Excel | Power BI
         </p>
+        <ul class="icons">
+        <li>
+          <a
+            href="https://www.linkedin.com/in/amandeep-s"
+            class="icon brands fa-linkedin-in"
+            ><span class="label">LinkedIn</span></a
+          >
+        </li>
+        <li>
+          <a
+            href="https://github.com/Amanespana"
+            class="icon brands fa-github"
+            ><span class="label">Github</span></a
+          >
+        </li>
+        <li>
+          <a
+            href="mailto:amandeep1552@gmail.com"
+            class="icon solid fa-envelope"
+            ><span class="label">Email</span></a
+          >
+        </li>
+      </ul>
+      <ul class="copyright">
+        <li>&copy; Amandeep Singh</li>
+        <li>Design by: <a href="http://html5up.net">HTML5 UP</a></li>
+      </ul>
       </div>
     </header>
     `
